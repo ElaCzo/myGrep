efff469b647f4f15ab3324843214328b7c7427b5
Ivan <delgado.ivan@protonmail.com>
2019-10-02 10:21:17 +0200

    best main

--- Indexing.java
+++ Indexing.java
@@ -14,115 +14,33 @@ import java.util.ArrayList;
 // caractère fréquent : (a, etc) : articl indéfini.
 // tou sles mots sup à une certaines fréquence : check par un humain.
 public class Indexing {
-    public static class Coordonnee {
-        public int ligne;
-        public int colonne;
 
-        public Coordonnee(int l, int c){
-            ligne = l;
-            colonne = c;
-        }
-    }
-
-    public static class Index{
-        ArrayList<Coordonnee> coordonnees;
+    public static class Index {
+        ArrayList<TextPosition> coordonnees;
         String mot;
         int frequence;
 
-        public Index(String mot, int frequence){
+        public Index(String mot, int frequence) {
             this.mot = mot;
-            this.frequence=frequence;
-            coordonnees = new ArrayList<Coordonnee>();
+            this.frequence = frequence;
+            coordonnees = new ArrayList<TextPosition>();
         }
 
-        public Index(String mot, int ligne, int colonne){
+        public Index(String mot, int ligne, int colonne) {
             this.mot = mot;
-            frequence=1;
-            coordonnees = new ArrayList<Coordonnee>();
+            frequence = 1;
+            coordonnees = new ArrayList<TextPosition>();
             coordonnees.add(new Coordonnee(ligne, colonne));
         }
 
-        public Index(String mot, int ligne, int colonne, int frequence){
+        public Index(String mot, int ligne, int colonne, int frequence) {
             this.mot = mot;
-            frequence=1;
+            frequence = 1;
             coordonnees = new ArrayList<Coordonnee>();
             coordonnees.add(new Coordonnee(ligne, colonne));
-            this.frequence=frequence;
+            this.frequence = frequence;
         }
 
-        public boolean equals(Object obj) {
-            if(obj instanceof String)
-                return (mot.equals((String)obj));
-            return false;
-        }
     }
 
-    // public static void main(String[] argv) throws IOException
-    // {
-    //     BufferedReader facteurs = null;
-    //     BufferedReader texte = null;
-
-    //     try
-    //     {
-    //         facteurs = new BufferedReader(new FileReader(".index"));
-    //         texte = new BufferedReader(new FileReader(argv[0]));
-    //     }
-    //     catch(FileNotFoundException exc)
-    //     {
-    //         System.out.println("Erreur d'ouverture");
-    //     }
-
-
-    //     /* Créaction de la table d'indexation avec uniquement les mots et leur fréquence.
-    //      * Il manque les coordonnées des mots. Elles seront mises à jour juste après. */
-    //     ArrayList<Index> tableDIndexation = new ArrayList<Index>();
-
-    //     String ligne, facteur;
-    //     String[] facteurEtFrequence;
-    //     int frequence;
-
-    //     // on crée la table d'indexation à partir des mots trouvés rangés par fréquence :
-    //     while ((ligne = facteurs.readLine()) != null) {
-    //         // On récupère les données du fichiers :  la fréquence et le facteur.
-    //         facteurEtFrequence = ligne.split(" ");
-    //         frequence = Integer.valueOf(facteurEtFrequence[0]);
-
-    //         /* si la fréquence du mot n'est pas trop élevée, on place le mot
-    //          * et sa fréquence dans la table d'indexation */
-    //         if(frequence < 800 /* /!\ A REGLER !!!! */) {
-    //             facteur = facteurEtFrequence[1];
-
-    //             Index i = new Index(facteur, frequence);
-    //             tableDIndexation.add(i);
-    //         }
-    //     }
-
-
-    //     /* Il reste la mise à jour des coordonnées : */
-    //     String[] mots;
-    //     int numeroLigne =0;
-    //     // while((ligne = texte.readLine()) != null) {
-    //     //     // A chaque changement de ligne, on augmente le numéro de la ligne
-    //     //     numeroLigne++;
-
-    //     //     // On récupère la liste de mots de la ligne
-    //     //     mots = ligne.split(" ");
-
-    //     //     for(int i=0 ; i<mots.length ; i++) {
-    //     //         // On récupère l'indice de la première occurence de facteur dans texte
-    //     //         int indice = KMP.kmp(ligne, mots[i].toCharArray(), KMP.retenue(mots[i].toCharArray()));
-
-    //     //         /* si le mot existe sur la ligne,
-    //     //         /* indice est un nombre positif représentant le numéro du caractère où le mot commence. */
-    //     //         if (indice > -1) {
-    //     //             // si c'est un mot qui mérite d'être dans la table d'indexation (pas trop fréquent)
-    //     //             if (tableDIndexation.contains(mots[i]))
-    //     //                 tableDIndexation.get(tableDIndexation.indexOf(mots[i])).coordonnees.add(new Coordonnee(numeroLigne, indice));
-    //     //         }
-    //     //     }
-    //     // }
-
-    //     facteurs.close();
-    //     texte.close();
-    // }
 }
--- myGrep.java
+++ myGrep.java
@@ -22,25 +22,57 @@ public class myGrep {
 
         }
 
-        ArrayList<String> text=null;
+        ArrayList<String> text = null;
 
         try {
-
             text = new ArrayList<>(Files.readAllLines(Paths.get(path)));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ArrayList<TextPosition> pos = KMP.kmp(text, regEx.toCharArray());
 
-        if (pos == null) {
+        ArrayList<TextPosition> pos = null;
+
+        if (indexable(regEx)) {
+
+        }
+        if (pos == null && kmpable(regEx)) {
+            pos = KMP.kmp(text, regEx.toCharArray());
+        } else {
+
+        }
+
+        if (pos == null || pos.size() == 0) {
             System.out.println("motif non trouvé");
         }
 
         else {
-            System.out.println("motif trouvé a la place : ");
+            System.out.println(pos.size() + " matchs trouvés : ");
             for (TextPosition p : pos) {
                 System.out.println(text.get(p.ligne));
             }
         }
+    }
+
+    public static boolean indexable(String facteur) {
+
+        for (char c : facteur.toCharArray()) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean kmpable(String facteur) {
+
+        for (char c : facteur.toCharArray()) {
+            if (c == '*' || c == '(' || c == ')' || c == '|') {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
